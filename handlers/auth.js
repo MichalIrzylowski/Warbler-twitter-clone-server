f9ac4598198d26b1c2c42214f7c9991e64b36132
Michał <michal.irzylowski@gmail.com>
2018-04-15 11:23:02 +0000

    ADD authentication

---
+++ handlers/auth.js
@@ -0,0 +1,18 @@
+const db = require("../models"),
+      jwt = require("jsonwebtoken");
+
+exports.signin = function () {
+  
+};
+
+exports.signup = async function (req, res, next) {
+  try {
+    //create a user
+    //create a token (signing a token)
+  } catch (err) {
+    //see what kind of error
+    //if it is a certain error
+    //respond with email already taken
+    //otherwise send a generic 400 error
+  }
+};
