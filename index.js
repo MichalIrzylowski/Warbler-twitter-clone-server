96b297f1c62038094f134482f4668cbb85b5d5b1
Michał <michal.irzylowski@gmail.com>
2018-04-15 10:43:03 +0000

    ADD index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express"),
+      app = express(),
+      cors = require("cors"),
+      bodyParser = require("body-parser");
+      
+const errorHandler = require("./handlers/error");
+      
+const PORT = 8081;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  let err = new Error('Not found!');
+  err.status = 404;
+  next(err);
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, function() {
+  console.log(`App is listening on port: ${PORT}`);
+});
