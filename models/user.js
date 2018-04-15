897974dad07290290cb326afe533c3afb5a76719
Michał <michal.irzylowski@gmail.com>
2018-04-15 11:10:36 +0000

    ADD hashing passwords and compare password function

--- models/user.js
+++ models/user.js
@@ -20,6 +20,30 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+//HASHING PASSWORDS
+userSchema.pre('save', async function(next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//HELPER FUNCTION
+userSchema.method.comparePasswords = async function (candidatePassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    return next(err);
+  }
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
