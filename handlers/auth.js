88f0c5e361f410b18d81e90914cdd4befa701d93
Michał <michal.irzylowski@gmail.com>
2018-04-15 11:40:34 +0000

    ADD authentication

--- handlers/auth.js
+++ handlers/auth.js
@@ -8,11 +8,31 @@ exports.signin = function () {
 exports.signup = async function (req, res, next) {
   try {
     //create a user
+    let user = await db.User.create(req.body);
+    let {id, username, profileImgUrl} = user;
+      //TOKEN - first we put some payload data (in object) like id, username and proflile img, than we put our secret key
+    let token = jwt.sign({
+      id,
+      username,
+      profileImgUrl
+    }, process.env.SECRET_KEY);
     //create a token (signing a token)
+    return res.status(200).json({
+      id,
+      username,
+      profileImgUrl,
+      token
+    });
   } catch (err) {
-    //see what kind of error
-    //if it is a certain error
-    //respond with email already taken
-    //otherwise send a generic 400 error
+    //if validation fails
+    if (err.code === 11000) {
+      err.message = 'Sorry, this email is already taken';
+    }
+    
+    return next({
+      status: 400,
+      message: err.message
+    });
+    
   }
 };
